850f1a0d8449e8908410071ae86c11c514ab9828
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-09 13:38:25 -0800

    save to localStorage

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,7 +7,7 @@ hours.forEach(function (hour) {
   hourDiv.classList.add("row", "time-block");
   hourDiv.innerHTML = `<div class="col-md-1 hour">${hour}:00</div>
   <textarea class="col-md-10 description ${timeColor(hour)}"> </textarea>
-  <button class="btn saveBtn col-md-1">
+  <button data-hour="${hour}" class="btn saveBtn col-md-1" onclick="saveText(this)">
     <i class="fas fa-save"></i>
   </button>`;
   containerEl.append(hourDiv);
@@ -21,3 +21,21 @@ function timeColor(hour) {
   else if (currentHour < hour) return "future";
   else return "past";
 }
+
+function saveText(button) {
+  console.log(button.dataset.hour);
+  console.log(button.parentNode.children[1].value);
+  localStorage.setItem(
+    button.dataset.hour,
+    button.parentNode.children[1].value
+  );
+}
+
+// var savedTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
+// var task = {
+//   hour: currentHour,
+//   initials: initials,
+// };
+// // save to localStorage
+// savedTasks.push(task);
+// localStorage.setItem("highscore", JSON.stringify(highscore));
