ec764c719a0075c524a3962a3404bae80069e6ff
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-09 12:58:46 -0800

    current hour and timeColor added

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,23 @@
 var containerEl = document.getElementById("scheduler");
-
+var currentDay = document.getElementById("current-day");
 var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
 
 hours.forEach(function (hour) {
   var hourDiv = document.createElement("div");
   hourDiv.classList.add("row", "time-block");
   hourDiv.innerHTML = `<div class="col-md-1 hour">${hour}:00</div>
-  <textarea class="col-md-10 description"> </textarea>
+  <textarea class="col-md-10 description ${timeColor(hour)}"> </textarea>
   <button class="btn saveBtn col-md-1">
     <i class="fas fa-save"></i>
   </button>`;
   containerEl.append(hourDiv);
 });
+currentDay.textContent = new Date().toLocaleDateString();
+console.log(new Date().getHours());
+
+function timeColor(hour) {
+  var currentHour = new Date().getHours();
+  if (currentHour == hour) return "present";
+  else if (currentHour < hour) return "future";
+  else return "past";
+}
