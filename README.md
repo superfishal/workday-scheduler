de2e027184d26fecf6de48961229c034b2332d56
superfishal <97133716+superfishal@users.noreply.github.com>
2022-03-10 12:24:21 -0800

    final commit

--- README.md
+++ README.md
@@ -1,7 +1,27 @@
 # Work Day Scheduler
 
-A daily scheduler application built with HTML/CSS, powered by jQuery, and Moment.js to work with date and time.
+A daily scheduler application built with HTML, CSS and Javascript to plan each day by hour from 0900-1700.
+
+# Criteria
+
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with time blocks for standard business hours
+WHEN I view the time blocks for that day
+THEN each time block is color-coded to indicate whether it is in the past, present, or future
+WHEN I click into a time block
+THEN I can enter an event
+WHEN I click the save button for that time block
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
 
 # Links
+
+Repository - https://github.com/superfishal/workday-scheduler
+
+Deployed Application - https://superfishal.github.io/workday-scheduler/
 
 # Deployed Application Screenshots
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,27 +1,33 @@
 var containerEl = document.getElementById("scheduler");
 var currentDay = document.getElementById("current-day");
 var hours = ["09", "10", "11", "12", "13", "14", "15", "16", "17"];
-
+// for each of the hours listed above
+// creates a div with classes and inner html generated for each hour
+// template literals ${hour} apply to each row
 hours.forEach(function (hour) {
   var hourDiv = document.createElement("div");
   hourDiv.classList.add("row", "time-block");
   hourDiv.innerHTML = `<div class="col-md-1 hour">${hour}:00</div>
-  <textarea class="col-md-10 description ${timeColor(hour)}"> </textarea>
+  <textarea class="col-md-10 description ${timeColor(hour)}">${
+    localStorage.getItem(hour) || ""
+  }</textarea>
   <button data-hour="${hour}" class="btn saveBtn col-md-1" onclick="saveText(this)">
     <i class="fas fa-save"></i>
   </button>`;
+  // appends to container
   containerEl.append(hourDiv);
 });
+// current day
 currentDay.textContent = new Date().toLocaleDateString();
 console.log(new Date().getHours());
-
+// checks color of each hour div to the currentHour
 function timeColor(hour) {
   var currentHour = new Date().getHours();
   if (currentHour == hour) return "present";
   else if (currentHour < hour) return "future";
   else return "past";
 }
-
+// run by saveBtn onclick function
 function saveText(button) {
   console.log(button.dataset.hour);
   console.log(button.parentNode.children[1].value);
@@ -30,12 +36,3 @@ function saveText(button) {
     button.parentNode.children[1].value
   );
 }
-
-// var savedTasks = JSON.parse(window.localStorage.getItem("tasks")) || [];
-// var task = {
-//   hour: currentHour,
-//   initials: initials,
-// };
-// // save to localStorage
-// savedTasks.push(task);
-// localStorage.setItem("highscore", JSON.stringify(highscore));
